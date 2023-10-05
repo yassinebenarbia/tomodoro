7331546d80c698c3005d93a31013d65e5a951105
yassinebenarbia <yassinebenarbia980@gmail.com>
2023-10-05 11:15:10 +0100

    partial implementation of the widget_fixer, as it will help setting the tomodoro app to be 'responsive'

--- src/widget_fixer.rs
+++ src/widget_fixer.rs
@@ -1,8 +1,18 @@
-#[derive(Debug)]
-// can put the backed here
-struct Fixer {}
+use tui::{Frame, backend::Backend};
+// desired behavior
+// let fixer = Fixer::new(f);
+// fixer.rrect(10, 20, 5, 10); // returns a Rect with width 5% and height 10%
+// of the screen, and on a position 10% and 20% respectivelf
+// some_widget.layout(fixer.xratio(20), fixer.yratio(10), 10, 20);
+// other_widget.layout(fixer.rrect(10, 20, 5, 10));
 
-impl Fixer {
+// #[derive(Debug)]
+// can put the backed here
+struct Fixer<'a, B: Backend> {
+    frame: &'a Frame<'a, B>
+}
+
+impl<'a, B: Backend> Fixer<'a, B> {
 
     pub fn ratio(x: u8, y: u8){
         if x > 100 {
@@ -16,7 +26,8 @@ impl Fixer {
         // use xratio and yratio respectively to extract the correct ratios
     }
 
-    pub fn xratio(x: u8){
+    pub fn xratio(&mut self, x: u16) -> u16{
+
         // access screen width
         // get the length of 1%
         // multiply that by the ratio x
@@ -24,9 +35,14 @@ impl Fixer {
             panic!("unable to provide x ratio bigger than 100\n
                 provided {} but expected a number less than 100", x);
         }
+        let size = self.frame.size();
+        // 20%
+        // size.x / 100 * 20%
+        size.x / 100 * x
     }
 
-    pub fn yratio(y: u8){
+    pub fn yratio(&mut self, y: u16) -> u16{
+
         // access screen height
         // get the length of 1%
         // multiply that by the ratio y
@@ -34,6 +50,12 @@ impl Fixer {
             panic!("unable to provide y ratio bigger than 100\n
                 provided {} but expected a number less than 100", y);
         }
+
+        let size = self.frame.size();
+        // 20%
+        // size.y / 100 * 20%
+        size.y / 100 * y
+
     }
 
 }
