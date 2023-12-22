3dbb0047cc77710bbfc35254b6e76c098d331618
Yassine <yassinebenarbia980@gmail.com>
2023-12-22 03:40:27 +0200

    temp

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
-  outputs = {self, nixpkgs, ...}:
+  outputs = {self, nixpkgs, lib, ...}:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell
 {
-
+  nativBuildInputs = with pkgs; [
+    hello
+    pkg-config
+    alsa-lib.dev
+  ];
+  shellHook = ''
+  echo "Hello, wellcome to your custom development shell"
+  export TOMODORO_PATH=$(pwd)
+  '';
 }
